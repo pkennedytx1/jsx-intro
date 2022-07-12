089ea946897a9209061a9415f29b0299cb61e43b
Patrick Kennedy <pkennedytx1@gmail.com>
2022-07-12 16:48:42 -0500

    added the component to be formatted into something else

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { data } from './data';
+
+const personCardComponent = ({ firstName, lastName, beachOrMountains }) => {
+  return(
+    <div>
+      <img
+        height='40px'
+        alt={beachOrMountains === 'Mountains' ? 'mountains' : 'beach'}
+        src={beachOrMountains === 'Mountains' ? 'mountain.svg' : 'beach.svg'}
+      />
+      <h2>{firstName} {lastName}</h2>
+    </div>
+  )
+} 
+
 function App() {
   return (
-    <div></div>
+    <div>
+      {data.map((data) => {
+        return(
+          personCardComponent(data)
+        )
+      })}
+    </div>
   );
 }
 
