e3f74f56c916182dc3c85cdab865c4e4c8c37bc8
Patrick Kennedy <pkennedytx1@gmail.com>
2022-07-15 15:28:29 -0500

    removed some files to avoid content overload and updated the data to be more acceptable for a workflow

--- src/data.js
+++ src/data.js
@@ -1,20 +1,24 @@
 export const data = [
     {
+        id: 1,
         firstName: 'Colette',
         lastName: 'Kennedy',
         beachOrMountains: 'Beach',
     },
-    {
+    {   
+        id: 2,
         firstName: 'Elisa',
         lastName: 'Lyon',
         beachOrMountains: 'Mountains',
     },
     {
+        id: 3,
         firstName: 'Patrick',
         lastName: 'Kennedy',
         beachOrMountains: 'Mountains',
     },
-    {
+    {   
+        id: 4,
         firstName: 'Isla',
         lastName: 'Kennedy',
         beachOrMountains: 'Beach',
