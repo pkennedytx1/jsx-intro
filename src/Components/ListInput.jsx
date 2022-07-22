fceccf94561672541e58742dcf4f771c68333b6c
Patrick Kennedy <pkennedytx1@gmail.com>
2022-07-21 21:48:09 -0500

    updated the form to contain more values so we can go over the validation

--- src/Components/ListInput.jsx
+++ src/Components/ListInput.jsx
@@ -3,12 +3,43 @@ import React from 'react';
 const ListInput = ({ handleNewPerson }) => {
     const [input, setInput] = React.useState();
     return(
-        <>
-            <input value={input} onChange={(e) => {
+        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '250px' }}>
+            <h2>Vacation Inquiry</h2>
+            <label for="first-name">First Name</label>
+            <input id='first-name' value={input} onChange={(e) => {
                 setInput(e.target.value);
             }} />
-            <button onClick={() => handleNewPerson(input)} >Add Person</button>
-        </>
+            <span style={{ margin: '5px' }} />
+            <label for="last-name">Last Name</label>
+            <input id='last-name' />
+            <span style={{ margin: '5px' }} />
+            <fieldset>
+                <legend>Select prefered vacation.</legend>
+                <div>
+                    <input
+                        type="radio"
+                        id="mountains"
+                        name="drone"
+                        value="Mountains"
+                    />
+                    <label for="mountains">Mountains</label>
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        id="beach"
+                        name="drone"
+                        value="Beach"
+                    />
+                    <label for="beach">Beach</label>
+                </div>
+            </fieldset>
+            <span style={{ margin: '5px' }} />
+            <label>Availability for appointment within the next seven days</label>
+            <input type='date' />
+            <span style={{ margin: '5px' }} />
+            <button onClick={() => handleNewPerson(input)} >Add Client Appointment</button>
+        </div>
     )
 }
 
