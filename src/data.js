c9dc3a242e2af2ccfc5aec502535009754e40aa8
Patrick Kennedy <pkennedytx1@gmail.com>
2022-07-21 21:52:12 -0500

    updated the data

--- src/data.js
+++ src/data.js
@@ -4,23 +4,27 @@ export const data = [
         firstName: 'Colette',
         lastName: 'Kennedy',
         beachOrMountains: 'Beach',
+        appointment: '2022-07-24T06:50:24.011Z'
     },
     {   
         id: 2,
         firstName: 'Elisa',
         lastName: 'Lyon',
         beachOrMountains: 'Mountains',
+        appointment: '2022-07-24T01:50:24.011Z'
     },
     {
         id: 3,
         firstName: 'Patrick',
         lastName: 'Kennedy',
         beachOrMountains: 'Mountains',
+        appointment: '2022-07-25T11:50:24.011Z'
     },
     {   
         id: 4,
         firstName: 'Isla',
         lastName: 'Kennedy',
         beachOrMountains: 'Beach',
+        appointment: '2022-07-27T11:50:24.011Z'
     }
 ]
