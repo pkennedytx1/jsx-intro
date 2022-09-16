fe128f15a9ab2f5d27fa5e496a64172f6d3a6428
Patrick Kennedy <pkennedytx1@gmail.com>
2022-09-16 13:21:13 -0500

    modeifed the list component

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,34 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ListItem from './ListItem';
 import ListInput from './ListInput';
 import { Spacer } from './ListItem.styles';
 import { ListContainer } from './List.styles';
-import axios from 'axios'
+import { data as initialData } from '../data';
 
 const List = ({ showAppointments = false }) => {
-    const [data, setData] = React.useState([]);
+    const [data, setData] = React.useState(initialData);
 
-    useEffect(() => {
-        setData([]);
-        axios
-            .get(`http://localhost:1337/api/appointments?token=${process.env.REACT_APP_API_TOKEN}`)
-            .then((response) => {
-                const apptData = response.data.data.map((appointment) => {
-                    return {
-                        id: appointment.id,
-                        firstName: appointment.attributes.first_name,
-                        lastName: appointment.attributes.last_name,
-                        vacationPreference: appointment.attributes.vacation_preference,
-                        vacationLocation: appointment.attributes.vacation_location,
-                        meetingDate: appointment.attributes.appointment_date
-                    }
-                })
-                setData(apptData);
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-    }, []);
     const handleNewPerson = ({ firstName, lastName, vacationLocation, vacationPreference, meetingDate }) => {
         const lastId = data[data.length - 1].id;
         setData([...data, {
